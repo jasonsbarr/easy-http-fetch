c5484cb60ea5c00cfe65cf215c11bece2db4e597
Jason Barr <jason@jasonsbarr.com>
2018-07-01 21:48:03 -0500

    Create put() and delete() methods

--- easyhttpfetch.js
+++ easyhttpfetch.js
@@ -2,6 +2,7 @@
  * Simple wrapper for Fetch to make asynchronous HTTP
  * requests. Uses the same API as EasyHTTPAjax but
  * using updated JS practices, e.g. ES6+ classes
+ * Only works with JSON for now, but will fix
  * 
  * @file Fetch API wrapper functions
  * @author Jason Barr <jason@jasonsbarr.com>
@@ -11,7 +12,7 @@
 
 class EasyHTTP {
     /**
-     * Make HTTP GET request
+     * Make HTTP GET request to retrieve record(s)
      * 
      * @param {string} url 
      */
@@ -24,6 +25,12 @@ class EasyHTTP {
         });
     }
 
+    /**
+     * Make HTTP POST request to create record
+     * 
+     * @param {string} url 
+     * @param {Object} data 
+     */
     post(url, data) {
         return new Promise((resolve, reject) => {
             fetch(url, {
@@ -35,6 +42,43 @@ class EasyHTTP {
             })
             .then(response => response.json())
             .then(data => resolve(data))
+            .catch(error => reject(error))
+        });
+    }
+
+    /**
+     * Make HTTP PUT request to update record
+     * 
+     * @param {string} url 
+     * @param {Object} data 
+     */
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(error => reject(error))
+        });
+    }
+
+    /**
+     * Make HTTP DELETE request to delete record
+     * 
+     * @param {string} url 
+     */
+    delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(response => response.json())
+            .then(data => resolve(true))
             .catch(error => reject(error))
         });
     }
